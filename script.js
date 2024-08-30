d0d17426ead8277913199ff60f6f571629a08e1b
Ayan Sarkar <131677081+AYANSARKAR2001@users.noreply.github.com>
2024-08-30 23:53:58 +0530

    Add files via upload
    Digital Clock created by me

---
+++ script.js
@@ -0,0 +1,14 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+setInterval(()=>{
+
+let currentTime = new Date();
+
+hrs.innerHTML = currentTime.getHours();
+min.innerHTML = currentTime.getMinutes();
+sec.innerHTML = currentTime.getSeconds();
+//currentTime.now()
+},1000)
+
